542f39bb0cb830545d52402fd6b5783d39188b36
Juan <j.mercedes@vissualmedia.com>
2013-01-14 16:16:37 -0400

    vainilla rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doc::Application.config.secret_token = 'cae4cd6d5c0886cfa834f966b626080335c941cb49ee7d168c9180de39cb5a959b4e6ea4761816a64647ed1d0c685c46ef8bb3453ddc2e8c3208cdcf48e18170'
